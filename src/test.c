d37df5cc48144513ed389ec30ffd8b2cf610a3ed
omtinez <omtinez@gmail.com>
2014-01-18 22:05:53 +0000

    removed jump instructions and buffers

--- src/test.c
+++ src/test.c
@@ -4,38 +4,38 @@
 
 int main(int argc, char* argv[])
 {
-	const char *begin,*end;
-	char sTemp[64];
-	const char *error = NULL;
-	TRex *x = trex_compile("x{2}yy",&error);
-	if(x) {
-		sprintf(sTemp,"AxxyyxxA");
+    const char *begin,*end;
+    char sTemp[64];
+    const char *error = NULL;
+    TRex *x = trex_compile("x{2}yy",&error);
+    if(x) {
+        sprintf(sTemp,"AxxyyxxA");
         if (trex_match(x, sTemp)) printf("match");
         else printf("no match");
 
         /*
-		if(trex_search(x,sTemp,&begin,&end))
-		{
-			int i,n = trex_getsubexpcount(x);
-			TRexMatch match;
-			for(i = 0; i < n; i++)
-			{
-				char t[200];
-				trex_getsubexp(x,i,&match);
-				sprintf(t,"[%%d]%%.%ds\n",match.len);
-				printf(t,i,match.begin);
-			}
-			printf("match! %d sub matches\n",trex_getsubexpcount(x));
-		}
-		else {
-			printf("no match!\n");
-		}
-        */
+        if(trex_search(x,sTemp,&begin,&end))
+        {
+            int i,n = trex_getsubexpcount(x);
+            TRexMatch match;
+            for(i = 0; i < n; i++)
+            {
+                char t[200];
+                trex_getsubexp(x,i,&match);
+                sprintf(t,"[%%d]%%.%ds\n",match.len);
+                printf(t,i,match.begin);
+            }
+            printf("match! %d sub matches\n",trex_getsubexpcount(x));
+        }
+        else {
+            printf("no match!\n");
+        }
 
-		trex_free(x);
-	}
-	else {
-		printf("compilation error [%s]!\n",error?error:"undefined");
-	}
-	return 0;
+        trex_free(x);
+        */
+    }
+    else {
+        printf("compilation error [%s]!\n",error?error:"undefined");
+    }
+    return 0;
 }
--- src/trex.h
+++ src/trex.h
@@ -14,16 +14,16 @@ typedef unsigned int TRexBool;
 typedef struct TRex TRex;
 
 typedef struct {
-	const char *begin;
-	int len;
+    const char *begin;
+    int len;
 } TRexMatch;
 
 TREX_API TRex *trex_compile(const char *pattern,const char **error);
 TREX_API void trex_free(TRex *exp);
 TREX_API TRexBool trex_match(TRex* exp,const char* text);
-TREX_API TRexBool trex_search(TRex* exp,const char* text, const char** out_begin, const char** out_end);
-TREX_API TRexBool trex_searchrange(TRex* exp,const char* text_begin,const char* text_end,const char** out_begin, const char** out_end);
-TREX_API int trex_getsubexpcount(TRex* exp);
-TREX_API TRexBool trex_getsubexp(TRex* exp, int n, TRexMatch *subexp);
+//TREX_API TRexBool trex_search(TRex* exp,const char* text, const char** out_begin, const char** out_end);
+//TREX_API TRexBool trex_searchrange(TRex* exp,const char* text_begin,const char* text_end,const char** out_begin, const char** out_end);
+//TREX_API int trex_getsubexpcount(TRex* exp);
+//TREX_API TRexBool trex_getsubexp(TRex* exp, int n, TRexMatch *subexp);
 
 #endif
